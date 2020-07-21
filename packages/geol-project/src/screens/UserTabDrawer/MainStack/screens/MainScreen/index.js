a1561f99b3601e9a81319b6299a3c58f540f86c6
Mariam <mac@Mariams-MacBook-Pro.local>
2020-07-21 10:32:17 +0400

    fix

--- packages/geol-project/src/screens/UserTabDrawer/MainStack/screens/MainScreen/index.js
+++ packages/geol-project/src/screens/UserTabDrawer/MainStack/screens/MainScreen/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import {View, Text} from 'react-native';
+import CardComponent from './CardComponent';
 
 function MainScreen() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Main Screen</Text>
+      <CardComponent />
     </View>
   );
 }
