c1eddb2c663b3ef2c639c680082c89c3f94de4f1
akhalkatsishvili <khatiaakhalkatsishvili@gmail.com>
2020-07-22 19:33:28 +0400

    delete react-native-date-picker

--- packages/geol-project/src/screens/UserTabDrawer/CarStack/screens/ DetailScreen/index.js
+++ packages/geol-project/src/screens/UserTabDrawer/CarStack/screens/ DetailScreen/index.js
@@ -1,37 +1,39 @@
 import * as React from 'react';
-import { View, Text, Button, ImageBackground, ScrollView, TouchableOpacity } from 'react-native';
-import DatePicker from 'react-native-date-picker'
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 
-
-import { isEmpty } from 'lodash';
-
+import {isEmpty} from 'lodash';
 
 function DetailScreen() {
-    const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState(new Date());
 
-    return (
-        <ScrollView style={{ backgroundColor: '#fff', flex: 1 }}>
-            {/* <ImageBackground source={Geol} style={{ width: '100%', height: 300 }} /> */}
-            <View style={{ paddingHorizontal: 16 }}>
-
-            </View>
-            <TouchableOpacity
-                onPress={() => signIn({ username, password })}
-                style={{
-                    backgroundColor: '#6BA07F',
-                    width: '100%',
-                    height: 44,
-                    borderRadius: 6,
-                    marginTop: 20,
-                }}>
-                <Button
-                    title="Book Now"
-                    color="#fff"
-                    disabled={!isEmpty(username) && !isEmpty(password) ? false : true}
-                />
-            </TouchableOpacity>
-        </ScrollView >
-    );
+  return (
+    <ScrollView style={{backgroundColor: '#fff', flex: 1}}>
+      {/* <ImageBackground source={Geol} style={{ width: '100%', height: 300 }} /> */}
+      <View style={{paddingHorizontal: 16}}></View>
+      <TouchableOpacity
+        onPress={() => signIn({username, password})}
+        style={{
+          backgroundColor: '#6BA07F',
+          width: '100%',
+          height: 44,
+          borderRadius: 6,
+          marginTop: 20,
+        }}>
+        <Button
+          title="Book Now"
+          color="#fff"
+          disabled={!isEmpty(username) && !isEmpty(password) ? false : true}
+        />
+      </TouchableOpacity>
+    </ScrollView>
+  );
 }
 
 export default DetailScreen;
