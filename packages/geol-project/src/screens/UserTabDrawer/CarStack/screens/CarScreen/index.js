672335f316960a10d797728ee04eebf1874a50ba
akhalkatsishvili <khatiaakhalkatsishvili@gmail.com>
2020-07-23 12:06:25 +0400

    send order

--- packages/geol-project/src/screens/UserTabDrawer/CarStack/screens/CarScreen/index.js
+++ packages/geol-project/src/screens/UserTabDrawer/CarStack/screens/CarScreen/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import {
   View,
   Text,
@@ -10,54 +10,101 @@ import {
   FlatList,
   StyleSheet,
   Image,
-  Alert
+  Alert,
 } from 'react-native';
 import DatePicker from './DatePicker';
-import { isEmpty} from 'lodash'
+import {isEmpty} from 'lodash';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
-import { AllVehiclesContext } from '../../../../../Context/AllVehiclesContext';
+import {AllVehiclesContext} from '../../../../../Context/AllVehiclesContext';
+import axios from 'axios';
 
-function CarScreen({ route, navigation }) {
-  const { allVehicles, updateAllVehicles } = useContext(AllVehiclesContext);
+function CarScreen({route, navigation}) {
+  const {allVehicles, updateAllVehicles} = useContext(AllVehiclesContext);
 
   const [, setDate] = React.useState(new Date());
   const handleChange = newDate => {
     setDate(newDate);
   };
 
-  const onSubmit = () => {
-    Alert.alert('Thank you','Booking was addded successfully.', [{ text: 'OK' }]);
-    navigation.navigate('CarSearchScreen');
-  }
+  // const onSubmit = () => {
+  //   Alert.alert('Thank you', 'Booking was addded successfully.', [
+  //     {text: 'OK'},
+  //   ]);
+  //   navigation.navigate('CarSearchScreen');
+  // };
+
+  const onSubmit = id => {
+    // axios.get('https://ciu2020.herokuapp.com/vehicle/list').then(response => {
+    //   console.log('ress data--->', response.data);
+    //   updateAllVehicles(response.data);
+    // });
+
+    axios
+      .post('https://ciu2020.herokuapp.com/route/create', {
+        vehicle: id,
+        user: '5f148928a49b341e5aa2b90b',
+        routeStart: new Date(),
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    // axios
+    //   .put(
+    //     'https://ciu2020.herokuapp.com/route/create',
+    //     ('vehicle': '5f187f3c26f607c61c3efd2b'),
+    //     ('user': '5f148928a49b341e5aa2b90b'),
+    //     ('routeStart': '2020-07-18T20:09:21.177Z'),
+    //     {headers: {'Content-Type': 'text/plain'}},
+    //   )
+    //   .then(r => console.log(r))
+    //   .catch(e => console.log(e));
+  };
 
   let item = route.params && route.params.data;
   return (
-    <ScrollView style={{ backgroundColor: '#fff', flex: 1, }}>
+    <ScrollView style={{backgroundColor: '#fff', flex: 1}}>
       <View style={styles.itemContainer}>
         <View style={styles.container}>
           <View style={styles.imageContainer}>
             <Image
-              source={{ uri: item.imageUrl }}
+              source={{uri: item.imageUrl}}
               style={styles.image}
               resizeMode="contain"
             />
           </View>
           <Text style={styles.itemText}>{item.type}</Text>
-          {!isEmpty(item.color) ? <Text style={styles.itemText}>color: {item.color}</Text>: null}
+          {!isEmpty(item.color) ? (
+            <Text style={styles.itemText}>color: {item.color}</Text>
+          ) : null}
           <Text style={styles.itemText}>model: {item.model}</Text>
           <Text style={styles.itemText}>price: {item.price}$</Text>
-         {!isEmpty(item.licenseNumber) ? <Text style={styles.itemText}>
-            Car Number: {item.licenseNumber}
-          </Text>
-          : null}
+          {!isEmpty(item.licenseNumber) ? (
+            <Text style={styles.itemText}>
+              Car Number: {item.licenseNumber}
+            </Text>
+          ) : null}
         </View>
       </View>
       <View style={{alignItems: 'center', paddingBottom: 15}}>
-      <View style={{ paddingHorizontal: 16, alignItems: 'center', justifyContent: 'center', height: 40, borderWidth: 1, borderRadius: 15, borderColor: '#6BA07F', width: '40%'}}>
+        <View
+          style={{
+            paddingHorizontal: 16,
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: 40,
+            borderWidth: 1,
+            borderRadius: 15,
+            borderColor: '#6BA07F',
+            width: '40%',
+          }}>
           <DatePicker handleChange={handleChange} />
         </View>
-        </View>
-      {item.location ?
+      </View>
+      {item.location ? (
         <Button
           title="See At Map"
           color="#6BA07F"
@@ -83,20 +130,28 @@ function CarScreen({ route, navigation }) {
             Linking.openURL(url);
           }}
         />
-        : null}
+      ) : null}
 
       <View style={{alignItems: 'center'}}>
-      <TouchableOpacity
-        onPress={() => onSubmit()}
-        style={{
-          backgroundColor: '#6BA07F',
-          width: '50%',
-          height: 44,
-          borderRadius: 6,
-          marginTop: 20,
-        }}>
-           <Text style={{color: '#fff', fontSize: 18, textAlign: 'center', lineHeight: 44}}>Finish</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => onSubmit(item._id)}
+          style={{
+            backgroundColor: '#6BA07F',
+            width: '50%',
+            height: 44,
+            borderRadius: 6,
+            marginTop: 20,
+          }}>
+          <Text
+            style={{
+              color: '#fff',
+              fontSize: 18,
+              textAlign: 'center',
+              lineHeight: 44,
+            }}>
+            Finish
+          </Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
